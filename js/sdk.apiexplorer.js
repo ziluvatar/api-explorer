05cd5e9d7d5c8c7420e87a87f2c8a15018d2ea7c
Alberto Pose <albertopose@gmail.com>
2014-02-11 14:06:21 -0300

    Making sure of having token before API Explorer loads.

--- js/sdk.apiexplorer.js
+++ js/sdk.apiexplorer.js
@@ -435,52 +435,67 @@ define(function (require) {
 
   var loadApi = function(settings) {
 
-  if (settings.isAuth) {
-    target = $('#sdk-auth-api-content');
-  } else {
-    target = $('#sdk-api-content');
-  }
-
-    if (!target.hasClass('converted')){
-      var top = $('body').scrollTop();
-
-      $('.markdown', target).each(function() {
-        $(this).markdown();
-
-        var path = $(this).data('path');
-        if (path) {
-          var markdown = $(this).html();
-          var verb = $(this).data('verb');
-          var desc = $(this).data('description');
-          var path2 = $(this).data('path2'); // use for duplicated endpoints (path)
-          $(this).html(apiItemTemplate({markdown: markdown, verb: verb, path: path, path2: path2, description: desc}));
+    var url = urljoin('https://' + settings.tenantDomain, '/oauth/token');
+      return $.ajax({
+        url: url,
+        type: 'POST',
+        contentType: 'application/x-www-form-urlencoded; charset=UTF-8',
+        data: {
+          client_id:     settings.clientId,
+          client_secret: settings.clientSecret,
+          grant_type:    'client_credentials'
         }
-      });
+      }).pipe(function (token) {
+        settings.accessToken = token.access_token;
+        $('.tokenme').html(token.access_token);
+      }).pipe(function () {
+        if (settings.isAuth) {
+          target = $('#sdk-auth-api-content');
+        } else {
+          target = $('#sdk-api-content');
+        }
 
-      target.addClass('converted');
-      $('body').scrollTop(top);
-    }
+      if (!target.hasClass('converted')){
+        var top = $('body').scrollTop();
 
-    loading(true);
+        $('.markdown', target).each(function() {
+          $(this).markdown();
 
-    if (settings.isAuth) {
-      loadClients(settings).then(withSettings(onClientChanged));
-      loadScopes(settings);
-      loadResponseTypes(settings);
-      loadProtocols(settings);
-    } else {
-      loadClients(settings)
-        .then(withSettings(onClientChanged, settings))
-        .then(withSettings(loadConnections, settings))
-        .then(withSettings(loadRules, settings))
-        .then(withSettings(loadUsers, settings));
-    }
+          var path = $(this).data('path');
+          if (path) {
+            var markdown = $(this).html();
+            var verb = $(this).data('verb');
+            var desc = $(this).data('description');
+            var path2 = $(this).data('path2'); // use for duplicated endpoints (path)
+            $(this).html(apiItemTemplate({markdown: markdown, verb: verb, path: path, path2: path2, description: desc}));
+          }
+        });
 
-    hookStrategySelector();
-    hookJsonTogglers();
+        target.addClass('converted');
+        $('body').scrollTop(top);
+      }
+
+      loading(true);
+
+      if (settings.isAuth) {
+        loadClients(settings).then(withSettings(onClientChanged));
+        loadScopes(settings);
+        loadResponseTypes(settings);
+        loadProtocols(settings);
+      } else {
+        loadClients(settings)
+          .then(withSettings(onClientChanged, settings))
+          .then(withSettings(loadConnections, settings))
+          .then(withSettings(loadRules, settings))
+          .then(withSettings(loadUsers, settings));
+      }
+
+      hookStrategySelector();
+      hookJsonTogglers();
 
 
-    loading(false);
+      loading(false);
+    });
   };
 
   var routes;
