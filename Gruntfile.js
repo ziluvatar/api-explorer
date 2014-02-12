6587bbb67c9ba087431dfb14cb33e305f4775907
Alberto Pose <albertopose@gmail.com>
2014-02-12 17:59:28 -0300

    Adding a couple of new Grunt tasks.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,11 +1,27 @@
 module.exports = function (grunt) {
 
   require('./tasks/ejs2amd')(grunt);
+  require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
 
   grunt.initConfig({
+    clean: [
+      'dist/'
+    ],
+    template: {
+      production: {
+        src: 'index.jade',
+        dest: 'dist/index.html',
+        variables: {
+          clientId:     process.env.AUTH0_CLIENT_ID,
+          clientSecret: process.env.AUTH0_CLIENT_SECRET,
+          isAuth:       process.env.AUTH0_AUTH_API || false
+        }
+      }
+    },
     connect: {
       server: {
         options: {
+          base: 'dist',
           protocol: 'https',
           port: 8443,
           passphrase: '',
@@ -13,12 +29,77 @@ module.exports = function (grunt) {
         },
       },
     },
+    less: {
+      production: {
+        options: {
+          paths: ['assets/css'],
+          cleancss: true,
+          customFumctions: {
+            'baseLineHeight': function () {
+              return '20px';
+            }
+          }
+        },
+        files: {
+          'dist/api-explorer.css': 'less/api-explorer.less'
+        }
+      }
+    },
     ejs2amd: {
       compile: {
         files: [{
           src: 'templates/*',
           dest: 'dist/templates'
         }]
+      }
+    },
+    requirejs: {
+      compile: {
+        options: {
+          almond: true,
+          baseUrl: '.',
+          'shim': {
+            'jquery': {
+              init: function() {
+                return window.$;
+              }
+            },
+            'bootstrap':        [ 'jquery' ],
+          },
+          include: ['api-explorer'],
+          generateSourceMaps: true,
+          preserveLicenseComments: false,
+          optimize: 'uglify2',
+          replaceRequireScript: [{
+            files: ['dist/index.html'],
+            module: 'main',
+            modulePath: '/js/api-explorer'
+          }],
+          paths: {
+            'jquery':           'js/jquery.wrapper',
+            'jsoneditor':       'bower_components/jsoneditor/jsoneditor',
+            'url-join':         'bower_components/url-join/lib/url-join',
+            'bootstrap':        'bower_components/bootstrap/docs/assets/js/bootstrap-collapse',
+            'jquerymd':         'js/jquery.markdown',
+            'showdown':         'bower_components/showdown/compressed/showdown',
+
+            'templates/sdk_auth':        'dist/templates/sdk_auth',
+            'templates/sdk':             'dist/templates/sdk',
+            'templates/sdk.api-method':  'dist/templates/sdk.api-method',
+
+            'js/sdk.apiexplorer':        'js/sdk.apiexplorer',
+            'js/sdk.auth':               'js/sdk.auth',
+            'js/sdk.AuthApiExecutors':   'js/sdk.AuthApiExecutors',
+            'js/sdk.ApiExecutors':       'js/sdk.ApiExecutors',
+
+            'js/showdown.extensions':    'js/showdown.extensions',
+
+            'js/models/model':                'js/models/model',
+            'js/models/clients':              'js/models/clients',
+            'js/models/client-connections':   'js/models/client-connections',
+          },
+          out: 'dist/api-explorer.js'
+        }
       }
     },
     s3: {
@@ -63,13 +144,7 @@ module.exports = function (grunt) {
     }
   });
 
-  // Loading dependencies
-  for (var key in grunt.file.readJSON('package.json').devDependencies) {
-    if (key !== 'grunt' && key.indexOf('grunt') === 0) {
-      grunt.loadNpmTasks(key);
-    }
-  }
-
-  grunt.registerTask('default', ['ejs2amd', 'connect:server']);
-  grunt.registerTask('cdn', [/*'build', 'copy:release', */'s3:clean', 's3:publish', 'invalidate_cloudfront:production']);
+  grunt.registerTask('build', ['clean', 'template', 'less', 'ejs2amd', 'requirejs']);
+  grunt.registerTask('default', ['build', 'connect:server']);
+  grunt.registerTask('cdn', ['build', 's3:clean', 's3:publish', 'invalidate_cloudfront:production']);
 };
