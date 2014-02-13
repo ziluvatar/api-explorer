b37b144f06e1825ba6c574881ddba68339eb54f1
Alberto Pose <albertopose@gmail.com>
2014-02-13 00:26:17 -0300

    Oops, handling ejs2amd async behavior properly.

--- tasks/ejs2amd.js
+++ tasks/ejs2amd.js
@@ -3,10 +3,11 @@ var mkdirp =  require('mkdirp');
 var ejs    =  require('ejs');
 var path   =  require('path');
 var ejsamd =  require('ejs-amd');
+var async  =  require('async');
 
 // Inspired by ejs-amd npm module :)
 
-function renderFile(grunt, settings, fromPath, toPath) {
+function renderFile(grunt, settings, fromPath, toPath, callback) {
   fs.readFile(fromPath, 'utf8', function(err, str){
     if (err) { throw err; }
 
@@ -28,11 +29,12 @@ function renderFile(grunt, settings, fromPath, toPath) {
     var dir = path.resolve(path.dirname(toPath));
 
     mkdirp(dir, 0755, function(err){
-      if (err) { throw err; }
+      if (err) { return callback(err); }
       var amdOutput = ejsamd.wrapAmd(fn);
       fs.writeFile(toPath, amdOutput, function(err){
-        if (err) { throw err; }
-        grunt.log.debug('  \033[90mejs -> AMD js: \033[36m%s\033[0m', toPath);
+        if (err) { return callback(err); }
+        grunt.log.writeln('  \033[90mejs -> AMD js: \033[36m%s\033[0m', toPath);
+        callback();
       });
     });
   });
@@ -41,14 +43,26 @@ function renderFile(grunt, settings, fromPath, toPath) {
 module.exports = function (grunt) {
   grunt.registerMultiTask('ejs2amd', 'convert ejs template to amd compatible js.', function () {
 
+    var done = this.async();
+
+    var filesToRender = [];
+
     this.files.forEach(function (file) {
       file.src.forEach(function (f) {
         var suffixRe = /\.ejs$/;
         if ( suffixRe.test(f) ) {
           var toPath = path.resolve('.', file.dest, path.basename(f, '.ejs') + '.js');
-          renderFile(grunt, {}, f, toPath );
+          filesToRender.push((function (grunt, opts, from, to) {
+            return function (callback) {
+              renderFile(grunt, opts, from, to, callback);
+            };
+          })(grunt, {}, f, toPath));
         }
       });
+    });
+
+    async.parallel(filesToRender, function (err) {
+      done(!err);
     });
 
     grunt.log.ok('ejs2amd finished');
