530f0f00777c29189a4a5001c846e5ece982cdd8
Alberto Pose <albertopose@gmail.com>
2014-02-27 17:01:02 -0300

    Setting readOnly property from gruntfile.

--- Gruntfile.js
+++ Gruntfile.js
@@ -80,7 +80,8 @@ module.exports = function (grunt) {
         variables: {
           clientId:     process.env.AUTH0_CLIENT_ID,
           clientSecret: process.env.AUTH0_CLIENT_SECRET,
-          isAuth:       process.env.AUTH0_AUTH_API || false
+          isAuth:       process.env.AUTH0_AUTH_API || false,
+          readOnly:     false
         }
       }
     },
