3ac6fa706b1ea03165dc73e51a841227db28d7c3
Alberto Pose <albertopose@gmail.com>
2014-03-04 23:34:20 -0300

    Fixes #28: Provide anchors for API accordions

--- js/scroll-to-anchors.js
+++ js/scroll-to-anchors.js
@@ -4,29 +4,56 @@ define(function (require) {
 
   function scrollToAnchors(enabled, target) {
     if (!enabled) {
-      $('a.header-anchor').hide();
-    } else {
-      var selectedElement = document.location.hash;
-      var selector = $(selectedElement, target);
+      var headerAnchor = $('a.header-anchor', target);
 
-      // If elements matching selectors where found
-      if (selector.length) {
+      // Hide link icons
+      $('i', headerAnchor).hide();
 
-        var elementToScroll = {
-          // if h2/h3 scroll to itself
-          h2:   selector.offset().top,
-          h3:   selector.offset().top,
+      // Prevent clicking on titles
+      headerAnchor.click(function () {
+        return false;
+      });
 
-          // if div (accordion-body) scroll to parent 
-          div:  selector.parent().offset().top
-        };
-
-        selector.collapse('show');
-        $('html, body').animate({
-          scrollTop: elementToScroll[selector.prop('tagName').toLowerCase()] || selector.offset().top
-        });
-      }
+      return;
     }
+
+    var selectedElement = document.location.hash;
+
+    // Restore the anchor to the selected API Method
+    if (selectedElement.indexOf('#!') === 0) {
+      selectedElement = selectedElement.slice(2);
+    }
+
+    var selector = $(selectedElement, target);
+
+    // If elements matching selectors where found
+    if (selector.length) {
+
+      var elementToScroll = {
+        // if h2/h3 scroll to itself
+        h2:   selector.offset().top,
+        h3:   selector.offset().top,
+
+        // if div (accordion-body) scroll to parent
+        div:  selector.parent().offset().top
+      };
+
+      selector.collapse('show');
+      $('html, body').animate({
+        scrollTop: elementToScroll[selector.prop('tagName').toLowerCase()] || selector.offset().top
+      });
+    }
+
+    var accordionBody =  $('.accordion-body', target);
+    accordionBody.on('show', function() {
+      var accordionGroup = $(this).closest('.accordion-group');
+      var accordionToggle = $('.accordion-toggle', accordionGroup);
+
+      // Add #! to the begining of each of the API Methods to prevent following the
+      // link to the anchor when clicking on it.
+      window.location.hash = '!'+ accordionToggle.attr('href');
+    });
+
 
   }
 
